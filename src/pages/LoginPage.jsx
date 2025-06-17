04465e2f60a2fbba37ae729bc640409640d801e7
Pooja-287 <pooja28.vdl@gmail.com>
2025-06-17 10:26:01 +0530

    Update LoginPage.jsx

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -36,7 +36,7 @@ const LoginPage = ({ setRole }) => {
     setLoading(true);
 
     try {
-      const res = await axios.post('http://localhost:5000/api/auth/login', {
+      const res = await axios.post(`${process.env.REACT_APP_API_URL}/api/auth/login`, {
         officeId,
         password,
         role,
